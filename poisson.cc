d3ca2259c66eb0b645005543f8e85cbc2b893fdc
repl.it user <replituser@example.com>
2020-11-25 09:49:07 +0000

    First commit after exercise 3a

--- poisson.cc
+++ poisson.cc
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-
+#include <cmath>
 
 double poisson(double mu, int k) {
     return 0;
@@ -10,11 +10,22 @@ double poisson(double mu, int k) {
 int main() {
     using namespace std;
 
+    vector<int> zaehler(11);
 
     ifstream fin("datensumme.txt");
+    ofstream fout("hist.txt");
+
     int n_i;
     for(int i = 0 ; i < 234 ; ++i) {
         fin >> n_i;
+        zaehler[n_i] += 1;
     }
     fin.close();
+
+    for(unsigned int k = 0; k < zaehler.size(); ++k) {
+      fout << k << " " << zaehler[k] << endl;
+      cout << k << ": " << zaehler[k] << endl;
+    }
+
+    fout.close();
 }
